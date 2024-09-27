cd95ef6dbdc425e7de70d2e843272fd5cb1f891f
pixelDriftadv <pixeldrift.adv@proton.me>
2024-09-27 23:20:25 +0530

    Update worst_person_ever.tsx

--- src/app/messages/worst_person_ever.tsx
+++ src/app/messages/worst_person_ever.tsx
@@ -15,7 +15,8 @@ https://paradoxmahakal.vercel.app/ <br/>
 https://asifzayn.vercel.app/ <br/>
 the heck you doing in my WebApp, stalking me you stranger <br/>
 also, Pallavi, heres a yt playlist check it out ~ <br/>
-{"https://youtube.com/playlist?list=PLcp1KqeP2Wdn0iKFB78zs7twjqezEt58Y"}
+{"https://youtube.com/playlist?list=PLcp1KqeP2Wdn0iKFB78zs7twjqezEt58Y"} <br/>
+     (make sure to also include this in the message itself)
 </h1>
 </main>
   );
