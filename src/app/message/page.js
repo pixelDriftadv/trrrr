a15656498991e9a4d85edde491571df24865ce8a
pixelDriftadv <pixeldrift.adv@proton.me>
2024-10-04 23:36:51 +0530

    Update page.js

--- src/app/message/page.js
+++ src/app/message/page.js
@@ -9,7 +9,7 @@ export default function Home() {
     but you just left talking everyone for me, thanks a lot! being honest a person like that deserves the same, they would truly be a match for a boy who shows up with a new girl every now and then, perfect match for them<br/>
     just because of them I never talked to any other gs, even SAB, in short she was gorgeous, tried talking to me for 6 times, but I like a rude person, never continued, biggest regret of my life, she was so cute, brilliant and stuff but still you really just tell me to talk to other gs to just completely destroy my awkwardness of talking to opposite people? I mean you truly trust me, not like that mf, {'"I don\'t trust boys", if you talk to other g then I wont talk to you, while they themselves talking to new boys everyday'}<br/>
     for real, youre giving me a life and they took life out of me, tbh you realise I worth something and they never understood my worth, they just thought me to be a loyal pet just for their beautiful face {"(which i hate from the core of my heart)"}<br/>
-    being completely honest, I feel better, really better with you than before with them, thank you so much
+    being completely honest, I feel better, really better with you than before with them, thank you so much, let me know whenever i could help you
     </h1>
 </main>
   );
