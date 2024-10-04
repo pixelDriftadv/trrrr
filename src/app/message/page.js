6d391e8caadadda6d1a313852137acb35cf42c65
pixelDriftadv <pixeldrift.adv@proton.me>
2024-10-04 23:41:55 +0530

    Update page.js

--- src/app/message/page.js
+++ src/app/message/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex text-white bg-black min-h-screen flex-col items-center justify-between py-24 px-8">
-   <h1 className="text-lg font-light">bruh pallavi youre the complete opposite of them <br/>
+   <h1 className="text-xs font-light">bruh pallavi youre the complete opposite of them <br/>
     i remember everytime i felt depressed and i went onto seeing their profile, everytime i mean every single time i saw a new boy and felt even more depressed, i even sometimes felt suicidal<br/>
     but you just left talking everyone for me, thanks a lot! being honest a person like that deserves the same, they would truly be a match for a boy who shows up with a new girl every now and then, perfect match for them<br/>
     just because of them I never talked to any other gs, even SAB, in short she was gorgeous, tried talking to me for 6 times, but I like a rude person, never continued, biggest regret of my life, she was so cute, brilliant and stuff but still you really just tell me to talk to other gs to just completely destroy my awkwardness of talking to opposite people? I mean you truly trust me, not like that mf, {'"I don\'t trust boys", if you talk to other g then I wont talk to you, while they themselves talking to new boys everyday'}<br/>
