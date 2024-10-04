29058e6bf70d0e2bb97e7293e392639f3ecdab7a
pixelDriftadv <pixeldrift.adv@proton.me>
2024-10-04 23:33:22 +0530

    Update page.js

--- src/app/message/page.js
+++ src/app/message/page.js
@@ -4,19 +4,13 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex text-white bg-black min-h-screen flex-col items-center justify-between py-24 px-8">
-   <h1 className="text-xl font-light">{"I don't have idea who you are, neither I want to know, but still, here's something he got for you"}</h1>
-   <h1 className="text-xl font-light">You better head on to <br/>
-     {"https://farhanahmad.vercel.app/"} <br/>
-     {"https://itzsahil.vercel.app/"} <br/>
-    {"https://itzkalam.vercel.app/"} <br/>
-     {"https://sahadathussain.vercel.app/"} <br/>
-     {"https://paradoxmahakal.vercel.app/"} <br/>
-     {"https://asifzayn.vercel.app/"} <br/>
-the heck you doing in my WebApp, stalking me you stranger <br/>
-also, Pallavi, heres a yt playlist check it out ~ <br/>
-{"https://youtube.com/playlist?list=PLcp1KqeP2Wdn0iKFB78zs7twjqezEt58Y"} <br/>
-     {"   (make sure to also include this in the message itself)"}
-</h1>
+   <h1 className="text-xl font-light">bruh pallavi youre the complete opposite of them <br/>
+    i remember everytime i felt depressed and i went onto seeing their profile, everytime i mean every single time i saw a new boy and felt even more depressed<br/>
+    but you just left talking everyone for me, thanks a lot! being honest a person like that deserves the same, they would truly be a match for q boy who shows up with a new girl every now and then, perfect match for them<br/>
+    just because of them I never talked to qny other gs, even SAB, in short she was gorgeous, tried talking to me for 6 times, but I like a rude person, never continued, biggest regret of my life, she was so cute, brilliant and stuff but still you really just tell me to talk to other gs to just completely destroy my awkwardness of talking to opposite people? I mean you truly trust me, not like that mf, {'"I don\'t trust boys", if you talk to other g then I wont talk to you, while they themselves talking to new boys everyday'}<br/>
+    for real, youre giving me a life and they took life out of me, tbh you realise I worth something and they never understood my worth, they just thought me to be a loyal pet just for their beautiful face {"(which i hate from the core of my heart)"}<br/>
+    being completely honest, I feel better, really better with you than before with them, thank you so much
+    </h1>
 </main>
   );
 }
