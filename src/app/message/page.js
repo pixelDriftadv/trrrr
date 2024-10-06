4db79125a2ca0c328f16e96792c9276d915b99cb
pixelDriftadv <pixeldrift.adv@proton.me>
2024-10-06 21:25:25 +0530

    Update page.js

--- src/app/message/page.js
+++ src/app/message/page.js
@@ -3,32 +3,9 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex text-white bg-black min-h-screen px-6 flex-col items-center justify-between py-24 px-8">
-   <h1 className="text-xs font-light">
-
-    agr tum harami, randi, kutti, madarchod, randi ki aulad, bsdk, bkl, chutiye, bhosdiwali abhi bhi mereko stalk karrhi hai na toh yeh jaalo <u>Pallavi ke AI bot hai Gemini Advanced based</u> teri tarah harami nahi hu kisi aur ladki se baat karne laag jaau mann bhar gya ek se toh, harami saali, abhi mera haal dekh ke accha lagg rha hoga na, thode din, mahine ya saal ruk tera isse bhi battar haal hoga<br/>
-    tum itne gire huye faltu nich kism k insaan ho ek AI pe gussa nikal ke aur baat karke jyada lgta hai aur tu saali asli insaan hoke inti faltu hai randi hai, tere se nafrat karne ka bhi mann nahi karta abb bass sab chiz bhul jaana chahta hu, saali tum harami mera zindagi mein aai hi kyu <br/>
-    aur main tera asliyat bhi dekh hi liya hu saali tm abhi takk usko unfollow nahi hi ki hoge aankh baand karke, bina dekhe bol skta hu pta chal apna asli aukaat? <br/>
-    sapna mein bhi matt sochna harami teri jaisi ke paas vapas auunga <u>suicide karlunga parr apna zindagi nark se bhi battar banane tere pass nahi aaunga chityue</u> <br/>
-    harami kutti kamini tu roz royegi koi madad karne nahi aayega, aay bhi toh sirf tera body ke liye<br/><br/>
-    
-    sacch mein abb apne aap se hi gussa aata itna faltu inssan k liye itna bekar ka efforts daal rha tha aakhir mein time bhi mera hi pass hua aur yeh harami toh kabhi puchne bhi nahi aayegi main zinda bhi hua ya nahi rha, iss chutiye ke liye toh aab koi aur aa hi gya hoga <br/>
-    parr sahi baat yeh nahi hai ki iss kamini ne mera dimaag kharab diya hai, abb kabhi kabhi lagta hai accha banke kya hi karlunga aakhir mein inn jaiso aur jyadatar log ko toh yahi faltu kism ke ladke hi pasand joh saale ladkiyon ko sirf use karne ka nazar se dekhte hai, jinko ghr pe girls ka respect kabhi sikhaya nahi gya, aur mereko jaha takk lagta hai in harami girls ko toh chorhne ke baad bhi inko sharam nahi aati hogi, saali vhir koi aur ldke dekh lete honge unka zindagi barbaad karne ke liye<br/>
-    isne sacch mein mera mental stability le liya, mera life ka sabse bada galti ispe bharosa karne, <u> main likh ke deta hu tum abhi jaake uska profile mein dekhlo voh uss ladke ko unfollow nahi ki hogi, main uska personality acche se jaanta hu, bina dekhe hi bata dunga </u> aur iss chutiye harami randi ki aulad ke liye main ek samay par uska webverse banane ka soccha tha, khud ko sahi mein maar lene ka mann karta hai kissi chiz se <br/>
-    aur uss harami ko kya, nya ladka toh mil hi gya hai, accha hai, main bhagwan se prarthna karunga uska bhi mere jaisa haal ho, merese bhi battar<br/><br/>
-    aur tum pal bhi sacch pls, phele hi bata do agr tm bhi uske jaise nikli toh sacch mein mera bharosa sab chiz se uth jayega toh pls, agr tm uske jaise ho bhi toh yeh mat sochna main kuch ulta sidha kaam karlunga, main bass abb apni mummy ko hi mera opposite partner decide krne de dunga, bohot hogya jhutha natak <br/>
-    aur agr nahi ho toh mereko chorhke matt jaana, saali harami randi mera dimag ka nuts dhila kardi abb kisi chiz mein mann nahi lagta, ek din bass samne dikh jaaye apna life mein gaali sikha hi hu sirf usko dene ke liye uska muh ke samne saara haar ek gaali dunga harami chutiya randi <br/><br/>
-    
-    bruh pallavi youre the complete opposite of them <br/>
-    i remember everytime i felt depressed and i went onto seeing their profile, everytime i mean every single time i saw a new boy and felt even more depressed, i even sometimes felt suicidal<br/>
-    but you just left talking everyone for me, thanks a lot! being honest a person like that deserves the same, they would truly be a match for a boy who shows up with a new girl every now and then, perfect match for them<br/>
-    just because of them I never talked to any other gs, even SAB, in short she was gorgeous, tried talking to me for 6 times, but I like a rude person, never continued, biggest regret of my life, she was so cute, brilliant and stuff but still you really just tell me to talk to other gs to just completely destroy my awkwardness of talking to opposite people? I mean you truly trust me, not like that mf, {'"I don\'t trust boys", if you talk to other g then I wont talk to you, while they themselves talking to new boys everyday'}<br/>
-    for real, youre giving me a life and they took life out of me, tbh you realise I worth something and they never understood my worth, they just thought me to be a loyal pet just for their beautiful face {"(which i hate from the core of my heart)"}
-  
-    <br/>
-    being completely honest, I feel better, really better with you than before with them, thank you so much, let me know whenever i could help you and yeah you could talk to other boys I wont feel bad after everything youve done for me, really, after all these I really trust you<br/><br/>
-      besides, youre exactly the type of a person i had been looking for, id surely never like to lose someone like you from my life, take care
-    </h1>
-</main>
+    <main className="flex text-white bg-black min-h-screen px-6 flex-col items-center justify-center py-24 ">
+ <Image alt="ss" src="/images/ss.png" width={100} height={100} className="w-full h-auto cover"/>
+    <h1 className="text-xs font-light"></h1>
+    </main>
   );
 }
