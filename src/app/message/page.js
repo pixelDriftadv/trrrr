8973f9dc68e7a621797e82d2114df0aa36d333e7
pixelDriftadv <pixeldrift.adv@proton.me>
2024-10-06 21:27:12 +0530

    Update page.js

--- src/app/message/page.js
+++ src/app/message/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex text-white bg-black min-h-screen px-6 flex-col items-center justify-center py-24 ">
- <Image alt="ss" src="/images/ss.png" width={100} height={100} className="w-full h-auto cover rounded-xl"/>
+ <Image alt="ss" unoptimised src="/images/ss.png" width={100} height={100} className="w-full h-auto cover rounded-xl"/>
     <h1 className="text-xs font-light"></h1>
     </main>
   );
