f0d2eca03fad87dbfb2c4475c0b746757b5d93a0
pixelDriftadv <pixeldrift.adv@proton.me>
2024-10-07 13:03:30 +0530

    Update page.js

--- src/app/message/page.js
+++ src/app/message/page.js
@@ -6,6 +6,9 @@ export default function Home() {
     <main className="flex text-white bg-black min-h-screen px-6 flex-col items-center justify-center py-24 ">
   <h1 className="text-xs font-light">
     array ab thodi uska profile kabhi dekhne jaata hu, main khud usko abb dusre ki property {"(ma*l)"} samjhta hu, jaa bhai teri hai tu hi rakh, mereko share bhi nhi krna, itna behuda kaam merese nhi hoga, 1 mahina toh isko apna asli aukat dikhane mein, abb life mein koi aur aayegi ya nahi ayegi <br/>
+   aur sacch batau toh mere liye bhi accha hai bhag gyi, iske jane ke baad jise mann usse baat karsakta hu bina kuch soche {"(mera skype, mastodon, whtsap, discord toh yh nhi dekh skti)"} aur toh aur iske jasne ke baad mera 2 webApps bna hai under 15 days, iske rehte huye 2 mnths mein 2 webs bnta tha, bohot time waste krdiya iske liye
+    <br/>
+    {'"When you know you\'re pure, then its not your loss, it\'s thiers"'} touched my heart
     </h1>
     </main>
   );
