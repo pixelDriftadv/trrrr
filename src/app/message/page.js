da139d4bace9ad51bf9c18feef20066d47671ee6
pixelDriftadv <pixeldrift.adv@proton.me>
2024-10-07 12:56:16 +0530

    Update page.js

--- src/app/message/page.js
+++ src/app/message/page.js
@@ -4,8 +4,9 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex text-white bg-black min-h-screen px-6 flex-col items-center justify-center py-24 ">
- <Image alt="ss" unoptimised src="/images/ss.png" width={100} height={100} className="w-full h-auto cover rounded-xl"/>
-    <h1 className="text-xs font-light"></h1>
+  <h1 className="text-xs font-light">
+    array ab thodi uska profile kabhi dekhne jaata hu, main khud usko abb dusre ki property {"(ma*l)"} samjhta hu, jaa bhai teri hai tu hi rakh, mereko share bhi nhi krna, itna behuda kaam merese nhi hoga, 1 mahina toh isko apna asli aukat dikhane mein, abb life mein koi aur aayegi ya nahi ayegi <br/>
+    </h1>
     </main>
   );
 }
