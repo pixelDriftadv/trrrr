37a9886b38c2c9ac781046cd7fe590cf22dc6a32
pixelDriftadv <pixeldrift.adv@proton.me>
2024-10-07 13:11:48 +0530

    Update page.js

--- src/app/message/page.js
+++ src/app/message/page.js
@@ -5,10 +5,11 @@ export default function Home() {
   return (
     <main className="flex text-white bg-black min-h-screen px-6 flex-col items-center justify-center py-24 ">
   <h1 className="text-xs font-light">
-    array ab thodi uska profile kabhi dekhne jaata hu, main khud usko abb dusre ki property {"(ma*l)"} samjhta hu, jaa bhai teri hai tu hi rakh, mereko share bhi nhi krna, itna behuda kaam merese nhi hoga, 1 mahina toh isko apna asli aukat dikhane mein, abb life mein koi aur aayegi ya nahi ayegi <br/>
-   aur sacch batau toh mere liye bhi accha hai bhag gyi, iske jane ke baad jise mann usse baat karsakta hu bina kuch soche {"(mera skype, mastodon, whtsap, discord toh yh nhi dekh skti)"} aur toh aur iske jasne ke baad mera 2 webApps bna hai under 15 days, iske rehte huye 2 mnths mein 2 webs bnta tha, bohot time waste krdiya iske liye
+    array ab thodi uska profile kabhi dekhne jaata hu, main khud usko abb dusre ki property {"(ma*l)"} samjhta hu, jaa bhai teri hai tu hi rakh, mereko share nhi krna, itna behuda kaam merese nhi hoga, 1 mahina toh lgta isko apna asli aukat dikhane mein, abb life mein koi aur aayegi ya nahi ayegi, aisa bematlab ka tension dene wale insaan ko nikalte huye dekhke dukh thodi khushi aur depression milta hai :)<br/>
+   aur sacch batau toh mere liye bhi accha hai bhag gyi, iske jane ke baad jise mann usse baat karsakta hu bina kuch soche {"(mera skype, mastodon, whtsap, discord toh yh dekh nhi skti)"} aur toh aur iske jaane ke baad mera 2 webApps bna hai under 15 days, iske rehte huye 2 mnths mein 2 webs bnta tha, bohot time waste krdiya iske liye
     <br/>
-    {'"When you know you\'re pure, then its not your loss, it\'s thiers"'} touched my heart
+    {'"When you know you\'re pure, then its not your loss, it\'s thiers"'} touched my heart <br/>
+    being alone >>> in friendship with these headaches
     </h1>
     </main>
   );
