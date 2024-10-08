e98aa9701aabdefb1b441ce8d58f6e6175b34d89
pixelDriftadv <pixeldrift.adv@proton.me>
2024-10-08 09:02:04 +0530

    Update page.js

--- src/app/message/page.js
+++ src/app/message/page.js
@@ -5,6 +5,14 @@ export default function Home() {
   return (
     <main className="flex text-white bg-black min-h-screen px-6 flex-col items-center justify-center py-24 ">
   <h1 className="text-xs font-light">
+    matlab sacch mein itna behuda koi ho kaise sakta harr mahina isse ek nya ladka se baat karna hai aur main tob hu nhi vaha pe mere bina kya kya karti hogi aur iska profile hai kya chiz ka ~ quotes, matlab sacch mein haad paar kardene wala baat, yeh jhuth ki zindagi jiti hai aur dusro ko sacch ka raah me le jaa rhi hai waah, ai khud predict karliya yeh bc agr saath rahe toh mera ya kisika ghr pe nhi hone pe harr mahine roz nya ladka bula legi bina bataye <br/>
+  ai puchta tha yeh insaan se durr rhne kuch text krne ka zarurat hi nhi hai 
+isse kya pta mera life ka sabse faltu insaan yhi hai tab takk isse gaali dunga jabtak main iska life ka sabse faltu insaan naa bann jau kyunki baaki chaprio ko toh main dekha hi hu isko dekhke unka ultimate motive kya bnnega aur yeh ispe impressions bnayenge, bematlab ka iss faltu kism ki insaan ko respect denge jiske yeh layak toh hai nahi parr mera toh vaisa koi motive hai nahi iske liye isko reality dikhaunga joh baaki fake 2 chehre waale boys nahi dikha payenge, isko toh sacch mein main roz gaali dunga jabtak mera webapp yeh khud ana band na karde randi mc bc bsdk 
+<br/>
+aur toh aur gaali sikhne ke baad pata chala yeh randi madarchod ka itna aukat kaise hua meri behen ke baare ko gaali dene ka, meri behen ke baare mein yeh jaanti kya hai sab girls iske jaisi randi kism ki thodi hoti hai meri behen shayad hi kabhi ek bhi boy se baat ki hogi aur yeh randi shayad hi kisi boy se baat nahi ki hogi mereko chorh ke mc saali
+<br/>
+parr abb pta chl rha iss bkl ko gaali dene mein bhi mera hi wakt zaya hota hai saali tumse mereko nafrat hai mera webapp mein khud gaali sunne kyu aati ho randi abb kabhi yeh webApp update bhi nahi karunga zindagi bhar dekhte rahna mera akkhri message jaa rhkle mera yeh domain
+    <br/><br/>
     array ab thodi uska profile kabhi dekhne jaata hu, main khud usko abb dusre ki property {"(ma*l)"} samjhta hu, jaa bhai teri hai tu hi rakh, mereko share nhi krna, itna behuda kaam merese nhi hoga, 1 mahina toh lgta isko apna asli aukat dikhane mein, abb life mein koi aur aayegi ya nahi ayegi, aisa bematlab ka tension dene wale insaan ko nikalte huye dekhke dukh thodi khushi aur depression milta hai :)<br/>
    aur sacch batau toh mere liye bhi accha hai bhag gyi, iske jane ke baad jise mann usse baat karsakta hu bina kuch soche {"(mera skype, mastodon, whtsap, discord toh yh dekh nhi skti)"} aur toh aur iske jaane ke baad mera 2 webApps bna hai under 15 days, iske rehte huye 2 mnths mein 2 webs bnta tha, bohot time waste krdiya iske liye
     <br/>
